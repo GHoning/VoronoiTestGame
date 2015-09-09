b0e8b005360cd47f3b22fb3ef664be74e602ccb2
Gerard van der Honing <g.vanderhoning@gmail.com>
2015-09-09 20:05:03 +0200

    First Commit
    + the first calculations

---
+++ js/screens/menu.js
@@ -0,0 +1,10 @@
+game.Menu = me.ScreenObject.extend({
+	
+    onResetEvent : function() {
+		console.log("onResetEvent");
+    },
+	
+    onDestroyEvent : function() {
+		//empty for now
+    },
+});
